aa861df3367cf7a2a787cde4a568a16a96ca0a33
Jan Mischak <jan.mischak@kstp.at>
2021-01-04 02:49:37 +0100

    Rename loop index variables in range-util

--- range-util.lua
+++ range-util.lua
@@ -9,15 +9,15 @@ local highest_chunk_range
 do
   local radar_range = 0
   local chunk_range = 2
-  for i = 1, radar_count do
+  for _ = 1, radar_count do
     local current_chunk_range = chunk_range - 1
     chunk_range = chunk_range * 2
 
     ranges[radar_range] = current_chunk_range
     radar_range = current_chunk_range
 
-    for i_chunk_range = current_chunk_range, chunk_range - 2 do
-      range_lookup[i_chunk_range] = current_chunk_range
+    for i = current_chunk_range, chunk_range - 2 do
+      range_lookup[i] = current_chunk_range
     end
   end
   highest_radar_range = radar_range / 2 + 1
